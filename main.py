ac942da7789ea3d039086f18cd016c5c37df2638
Usman <udandina@yahoo.com>
2025-09-22 19:53:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from text_utils import count_words, reverse_words, make_uppercase
+from math_utils import add, subtract, multiply, divide  
+from file_utils import read_file, save_to_file
 
 choice = input("what do you want to do? (text, math, file): ").lower()
 
@@ -11,6 +13,34 @@ if choice == "text" :
     print(reverse_words(text))
   elif operation =="uppercase" :
     print(make_uppercase(text))
-  else
+  else :
     print('\nInvalid Input!')
     
+elif choice == "math" :
+  operation =input("which operation? (add, subtract, multiply, divide): ").lower()
+  num1 = float(input("Enter num1: "))
+  num2 = float(input("Enter num2: "))
+  if operation == "add" :
+    print(num1+num2)
+  elif operation == "subtract" :
+    print(num1-num2)
+  elif operation =="multiply" :
+    print(num1*num2)
+  elif operation =="divide" :
+    print(num1/num2)
+  else :
+    print('\nInvalid Input!')
+
+elif choice == "file":
+    operation = input("Which operation? (save, read): ").lower()
+    filename = input("Enter filename: ")
+
+    if operation == "save":
+        text = input("Enter text to save: ")
+        save_to_file(filename, text)
+    elif operation == "read":
+        read_file(filename)   
+    else:
+        print('\nInvalid Input!')
+
+
